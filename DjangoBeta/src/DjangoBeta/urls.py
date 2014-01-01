6a96f69ab67a4b967dd4e79c8d2a1d90b0684fd2
Len Coia <len.coia@gmail.com>
2014-01-01 14:27:51 -0500

    testing commits

--- DjangoBeta/src/DjangoBeta/urls.py
+++ DjangoBeta/src/DjangoBeta/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+# test 
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'DjangoBeta.views.home', name='home'),
